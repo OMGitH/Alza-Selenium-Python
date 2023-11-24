eeb6c6739fa21ba808bd2f58c76f834e47dfa5ff
OMGitH <malinik@volny.cz>
2023-11-24 14:54:07 +0100

    assertions: Implemented mixed assertions that work as soft assertions but each assertion can be set to hard assertion.

--- Python_Selenium/Tests/tests_Alza.py
+++ Python_Selenium/Tests/tests_Alza.py
@@ -10,6 +10,7 @@ from Page_Objects.watchdog_add_dialog import WatchdogAdd
 from Page_Objects.delivery_addresses_page import DeliveryAddresses
 import pytest
 from pytest_check import check as soft_assert
+import mixed_assertions as mixed_assert
 
 
 @pytest.mark.usefixtures("initialize_driver")
@@ -34,7 +35,7 @@ class TestsAlza:
 
         # Reject all cookies.
         self.cookies_pane.cookies_pane_click_reject_all()
-        soft_assert.is_true(self.cookies_pane.cookies_pane_is_invisible(), "Cookies pane is still visible but shall not be.")
+        mixed_assert.is_true(self.cookies_pane.cookies_pane_is_invisible(), "Cookies pane is still visible but shall not be.")
 
         # Click login link.
         self.top_section.top_section_click_login_link()
@@ -42,38 +43,38 @@ class TestsAlza:
         # Unsuccessful login:
         # Email and password fields blank.
         self.login_page.login_click_signin_button()
-        soft_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.")
+        mixed_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.", True)
         actual_blank_email_text = self.login_page.login_get_blank_email_text()
-        soft_assert.equal(actual_blank_email_text, TestData.blank_email_text, f"Wrong message for blank e-mail input field. Actual message is '{actual_blank_email_text}' but it shall be '{TestData.blank_email_text}'.")
+        mixed_assert.equal(actual_blank_email_text, TestData.blank_email_text, f"Wrong message for blank e-mail input field. Actual message is '{actual_blank_email_text}' but it shall be '{TestData.blank_email_text}'.")
         actual_blank_password_text = self.login_page.login_get_blank_password_text()
-        soft_assert.equal(actual_blank_password_text, TestData.blank_password_text, f"Wrong message for blank password input field. Actual message is '{actual_blank_password_text}' but it shall be '{TestData.blank_password_text}'.")
+        mixed_assert.equal(actual_blank_password_text, TestData.blank_password_text, f"Wrong message for blank password input field. Actual message is '{actual_blank_password_text}' but it shall be '{TestData.blank_password_text}'.")
         # Wrong email and correct password provided.
         self.login_page.login_provide_email(TestData.incorrect_user_name)
         self.login_page.login_provide_password(TestData.password)
         self.login_page.login_click_signin_button()
-        soft_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.")
+        mixed_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.", True)
         actual_disabled_login_button_text = self.login_page.login_get_disabled_login_button_text()
-        soft_assert.equal(actual_disabled_login_button_text, TestData.signin_button_incorrect_user_name_password_text, f"Wrong message at signin button when incorrect e-mail provided. Actual message is '{actual_disabled_login_button_text}' but it shall be '{TestData.signin_button_incorrect_user_name_password_text}'.")
+        mixed_assert.equal(actual_disabled_login_button_text, TestData.signin_button_incorrect_user_name_password_text, f"Wrong message at signin button when incorrect e-mail provided. Actual message is '{actual_disabled_login_button_text}' but it shall be '{TestData.signin_button_incorrect_user_name_password_text}'.")
         # Correct email and wrong password provided.
         self.login_page.login_provide_email(TestData.user_name)
         self.login_page.login_provide_password(TestData.incorrect_password)
         self.login_page.login_click_signin_button()
-        soft_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.")
+        mixed_assert.is_true(self.login_page.login_dialog_is_visible(), "Login dialog is not visible though it shall be.", True)
         actual_disabled_login_button_text = self.login_page.login_get_disabled_login_button_text()
-        soft_assert.equal(actual_disabled_login_button_text, TestData.signin_button_incorrect_user_name_password_text, f"Wrong message at signin button when incorrect password provided. Actual message is '{actual_disabled_login_button_text}' but it shall be '{TestData.signin_button_incorrect_user_name_password_text}'.")
+        mixed_assert.equal(actual_disabled_login_button_text, TestData.signin_button_incorrect_user_name_password_text, f"Wrong message at signin button when incorrect password provided. Actual message is '{actual_disabled_login_button_text}' but it shall be '{TestData.signin_button_incorrect_user_name_password_text}'.")
 
         # Successful login.
         self.login_page.login_provide_email(TestData.user_name)
         self.login_page.login_provide_password(TestData.password)
         self.login_page.login_click_signin_button()
-        soft_assert.is_true(self.login_page.login_dialog_is_invisible(), "Login dialog is still visible but shall not be.")
+        mixed_assert.is_true(self.login_page.login_dialog_is_invisible(), "Login dialog is still visible but shall not be.", True)
         actual_signed_in_text = self.top_section.top_section_get_signed_in_user_text()
-        soft_assert.equal(actual_signed_in_text, TestData.user_signed_in_text, f"Wrong text in the top menu. Actual text is '{actual_signed_in_text}' but shall be '{TestData.user_signed_in_text}'. Seems user is not logged in though shall be.")
+        mixed_assert.equal(actual_signed_in_text, TestData.user_signed_in_text, f"Wrong text in the top menu. Actual text is '{actual_signed_in_text}' but shall be '{TestData.user_signed_in_text}'. Seems user is not logged in though shall be.")
 
         # Logout.
         self.top_section.top_section_click_signed_in_user_link()
         self.top_section.top_section_click_logout_link()
-        soft_assert.is_true(self.top_section.top_section_login_link_is_visible(), "Login link is not visible though it shall be.")
+        mixed_assert.is_true(self.top_section.top_section_login_link_is_visible(), "Login link is not visible though it shall be.")
 
     def test_basket_add_remove_item(self):
         """
@@ -114,24 +115,24 @@ class TestsAlza:
         self.main_page.main_page_click_first_computer_put_to_basket_button()
         # Check there is correct number at basket icon and go to basket.
         actual_number_of_items_at_basket_icon = self.top_section.top_section_get_number_of_items_at_basket_icon()
-        soft_assert.equal(actual_number_of_items_at_basket_icon, TestData.number_of_items_in_basket, f"Wrong number of items at basket icon. Actual number is '{actual_number_of_items_at_basket_icon}' but it shall be '{TestData.number_of_items_in_basket}'.")
+        mixed_assert.equal(actual_number_of_items_at_basket_icon, TestData.number_of_items_in_basket, f"Wrong number of items at basket icon. Actual number is '{actual_number_of_items_at_basket_icon}' but it shall be '{TestData.number_of_items_in_basket}'.")
         self.top_section.top_section_click_basket_icon()
 
         # On basket_page page:
         # Check item name, count and price.
         actual_computer_name = self.basket_page.basket_get_item_name()
-        soft_assert.is_in(first_computer_name, actual_computer_name, f"Wrong name of computer in basket. Actual name is '{actual_computer_name}' but it shall be '{first_computer_name}'. Seems computer that shall be in basket is not.")
+        mixed_assert.is_in(first_computer_name, actual_computer_name, f"Wrong name of computer in basket. Actual name is '{actual_computer_name}' but it shall be '{first_computer_name}'. Seems computer that shall be in basket is not.")
         actual_number_of_computers_in_basket = self.basket_page.basket_get_item_count()
-        soft_assert.equal(actual_number_of_computers_in_basket, TestData.number_of_items_in_basket, f"Wrong number of computers in basket. There are '{actual_number_of_computers_in_basket}' computers but there shall be '{TestData.number_of_items_in_basket}' computer.")
+        mixed_assert.equal(actual_number_of_computers_in_basket, TestData.number_of_items_in_basket, f"Wrong number of computers in basket. There are '{actual_number_of_computers_in_basket}' computers but there shall be '{TestData.number_of_items_in_basket}' computer.")
         actual_computer_price_in_basket = self.basket_page.basket_get_item_price()
-        soft_assert.equal(actual_computer_price_in_basket, first_computer_price, f"Wrong computer price in basket. Actual price is '{actual_computer_price_in_basket}' but shall be '{first_computer_price}'.")
+        mixed_assert.equal(actual_computer_price_in_basket, first_computer_price, f"Wrong computer price in basket. Actual price is '{actual_computer_price_in_basket}' but shall be '{first_computer_price}'.")
         # Remove item from basket and check it is empty.
         self.basket_page.basket_remove_all_items_from_basket()
         actual_text_once_basket_empty = self.basket_page.basket_get_text_once_all_items_removed()
-        soft_assert.equal(actual_text_once_basket_empty, TestData.text_once_all_items_removed_from_basket, f"Wrong text once basket is empty. Actual text is '{actual_text_once_basket_empty}' but it shall be '{TestData.text_once_all_items_removed_from_basket}'. Seems basket is not empty.")
+        mixed_assert.equal(actual_text_once_basket_empty, TestData.text_once_all_items_removed_from_basket, f"Wrong text once basket is empty. Actual text is '{actual_text_once_basket_empty}' but it shall be '{TestData.text_once_all_items_removed_from_basket}'. Seems basket is not empty.")
         # Check there is no number at basket icon.
         actual_number_of_items_at_basket_icon = self.top_section.top_section_get_number_of_items_at_basket_icon()
-        soft_assert.equal(actual_number_of_items_at_basket_icon, "No items", f"Wrong number of items at basket icon. Actual number is '{actual_number_of_items_at_basket_icon}' but there shall be no items.")
+        mixed_assert.equal(actual_number_of_items_at_basket_icon, "No items", f"Wrong number of items at basket icon. Actual number is '{actual_number_of_items_at_basket_icon}' but there shall be no items.")
 
         # Logout.
         self.top_section.top_section_click_signed_in_user_link()
@@ -165,15 +166,15 @@ class TestsAlza:
         self.top_section.top_section_click_search_button()
         # Check result.
         actual_search_result_title = self.main_page.main_page_get_search_result_header()
-        soft_assert.equal(actual_search_result_title, TestData.search_result_header_via_search_button, f"Wrong header of looked up section is displayed. Actual header is '{actual_search_result_title}' but it shall be '{TestData.search_result_header_via_search_button}'. Seems wrong section is displayed.")
-        soft_assert.greater(self.main_page.main_page_get_search_result_items_amount(), 0, "No items found, items shall be found.")
+        mixed_assert.equal(actual_search_result_title, TestData.search_result_header_via_search_button, f"Wrong header of looked up section is displayed. Actual header is '{actual_search_result_title}' but it shall be '{TestData.search_result_header_via_search_button}'. Seems wrong section is displayed.")
+        mixed_assert.greater(self.main_page.main_page_get_search_result_items_amount(), 0, "No items found, items shall be found.")
 
         # Search for "recenze" and choose from suggestion:
         self.top_section.top_section_search_provide_value(TestData.search_value_via_suggestion)
         self.top_section.top_section_search_suggestion_click_1st_item()
         # Check result.
         actual_search_result_title = self.main_page.main_page_get_search_result_header().lower()
-        soft_assert.is_in(TestData.search_result_word_in_title_via_suggestion, actual_search_result_title, f"Result doesn't contain looked up word in title. Actual title is '{actual_search_result_title}', it does not contain word '{TestData.search_result_word_in_title_via_suggestion}' though it shall.")
+        mixed_assert.is_in(TestData.search_result_word_in_title_via_suggestion, actual_search_result_title, f"Result doesn't contain looked up word in title. Actual title is '{actual_search_result_title}', it does not contain word '{TestData.search_result_word_in_title_via_suggestion}' though it shall.")
 
         # Logout.
         self.top_section.top_section_click_signed_in_user_link()
@@ -220,7 +221,7 @@ class TestsAlza:
         self.main_page.main_page_click_watch_price()
         # Check prefilled e-mail address.
         actual_email = self.watchdog_add_dialog.watchdog_add_dialog_get_email()
-        soft_assert.equal(actual_email, TestData.user_name, f"Incorrect e-mail address prefilled. There is '{actual_email}' but there shall be '{TestData.user_name}'.")
+        mixed_assert.equal(actual_email, TestData.user_name, f"Incorrect e-mail address prefilled. There is '{actual_email}' but there shall be '{TestData.user_name}'.")
         # Set watch price.
         self.watchdog_add_dialog.watchdog_add_dialog_set_price_limit(TestData.watchdog_price_limit)
         self.watchdog_add_dialog.watchdog_add_dialog_click_confirm_button()
@@ -232,14 +233,14 @@ class TestsAlza:
         self.my_account_page.my_account_click_watchdogs_link()
         # Check watched item name and price limit, checkbox alert price is checked.
         actual_pet_supply_name = self.watchdogs_page.watchdogs_get_watchdog_item_name()
-        soft_assert.is_in(first_pet_supply_name, actual_pet_supply_name, f"Wrong name of pet supply in watchdogs. Actual pet supply name is '{actual_pet_supply_name}' but it shall be '{first_pet_supply_name}'. Seems pet supply that shall be in watchdogs is not.")
+        mixed_assert.is_in(first_pet_supply_name, actual_pet_supply_name, f"Wrong name of pet supply in watchdogs. Actual pet supply name is '{actual_pet_supply_name}' but it shall be '{first_pet_supply_name}'. Seems pet supply that shall be in watchdogs is not.")
         actual_price_limit = self.watchdogs_page.watchdogs_get_price_limit_provided()
-        soft_assert.equal(actual_price_limit, TestData.watchdog_price_limit, f"Wrong price limit displayed in watchdogs. Actual price limit is '{actual_price_limit}' but it shall be '{TestData.watchdog_price_limit}'.")
-        soft_assert.is_true(self.watchdogs_page.watchdogs_check_alert_price_is_checked(), f"Checkbox for alert when price is lower than '{TestData.watchdog_price_limit}' shall be checked but it is not.")
+        mixed_assert.equal(actual_price_limit, TestData.watchdog_price_limit, f"Wrong price limit displayed in watchdogs. Actual price limit is '{actual_price_limit}' but it shall be '{TestData.watchdog_price_limit}'.")
+        mixed_assert.is_true(self.watchdogs_page.watchdogs_check_alert_price_is_checked(), f"Checkbox for alert when price is lower than '{TestData.watchdog_price_limit}' shall be checked but it is not.")
         # Remove item from watchdog list.
         self.watchdogs_page.watchdogs_remove_all_items_from_watchdogs_list()
         actual_text_once_watchdog_list_empty = self.watchdogs_page.watchdogs_get_text_once_all_items_removed()
-        soft_assert.equal(actual_text_once_watchdog_list_empty, TestData.text_once_all_items_removed_from_watchdog_list, f"Wrong text once watchdog list is empty. Actual text is '{actual_text_once_watchdog_list_empty}' but it shall be '{TestData.text_once_all_items_removed_from_watchdog_list}'. Seems watchdog list is not empty.")
+        mixed_assert.equal(actual_text_once_watchdog_list_empty, TestData.text_once_all_items_removed_from_watchdog_list, f"Wrong text once watchdog list is empty. Actual text is '{actual_text_once_watchdog_list_empty}' but it shall be '{TestData.text_once_all_items_removed_from_watchdog_list}'. Seems watchdog list is not empty.")
 
         # Logout.
         self.top_section.top_section_click_signed_in_user_link()
@@ -289,9 +290,9 @@ class TestsAlza:
         # Check number of addresses and their data.
         actual_number_of_delivery_addresses = self.delivery_addresses_page.delivery_addresses_get_number_of_addresses()
         expected_number_of_delivery_addresses = len(TestData.delivery_addresses_original)
-        soft_assert.equal(actual_number_of_delivery_addresses, expected_number_of_delivery_addresses, f"Incorrect number of delivery addresses. Actual number is '{actual_number_of_delivery_addresses}' but it shall be '{expected_number_of_delivery_addresses}'.")
+        mixed_assert.equal(actual_number_of_delivery_addresses, expected_number_of_delivery_addresses, f"Incorrect number of delivery addresses. Actual number is '{actual_number_of_delivery_addresses}' but it shall be '{expected_number_of_delivery_addresses}'.")
         actual_delivery_addresses_data = self.delivery_addresses_page.delivery_addresses_get_addresses_data(actual_number_of_delivery_addresses)
-        soft_assert.equal(actual_delivery_addresses_data, TestData.delivery_addresses_original, f"Actual delivery addresses are not the same as provided delivery addresses. Actual delivery addresses are '{actual_delivery_addresses_data}', expected delivery addresses are '{TestData.delivery_addresses_original}'.")
+        mixed_assert.equal(actual_delivery_addresses_data, TestData.delivery_addresses_original, f"Actual delivery addresses are not the same as provided delivery addresses. Actual delivery addresses are '{actual_delivery_addresses_data}', expected delivery addresses are '{TestData.delivery_addresses_original}'.")
 
         # Editing delivery addresses:
         self.delivery_addresses_page.delivery_addresses_edit_addresses()
@@ -303,13 +304,13 @@ class TestsAlza:
         self.my_account_page.my_account_click_delivery_addresses_link()
         # Check addresses have correct edited data.
         actual_delivery_addresses_data = self.delivery_addresses_page.delivery_addresses_get_addresses_data(actual_number_of_delivery_addresses)
-        soft_assert.equal(actual_delivery_addresses_data, TestData.delivery_addresses_edited, f"Actual delivery addresses are not the same as updated delivery addresses. Actual delivery addresses are '{actual_delivery_addresses_data}', expected delivery addresses are '{TestData.delivery_addresses_edited}'.")
+        mixed_assert.equal(actual_delivery_addresses_data, TestData.delivery_addresses_edited, f"Actual delivery addresses are not the same as updated delivery addresses. Actual delivery addresses are '{actual_delivery_addresses_data}', expected delivery addresses are '{TestData.delivery_addresses_edited}'.")
 
         # Remove delivery addresses:
         self.delivery_addresses_page.delivery_addresses_remove_all_addresses_from_delivery_addresses_list()
         # Check there are no addresses.
         actual_number_of_delivery_addresses = self.delivery_addresses_page.delivery_addresses_get_number_of_addresses()
-        soft_assert.equal(actual_number_of_delivery_addresses, 0, f"There are delivery addresses present though there shall not be any. There are '{actual_number_of_delivery_addresses}' delivery addresses.")
+        mixed_assert.equal(actual_number_of_delivery_addresses, 0, f"There are delivery addresses present though there shall not be any. There are '{actual_number_of_delivery_addresses}' delivery addresses.")
 
         # Logout.
         self.top_section.top_section_click_signed_in_user_link()
---
+++ Python_Selenium/mixed_assertions.py
@@ -0,0 +1,35 @@
+from pytest_check import check
+import pytest
+
+"""
+Mixed assertions use pytest_check soft assertions.
+Mixed assertions add interrupt_test argument, once set to True if assertion fails rest of test is skipped. So assertion
+behaves as a hard assertion. 
+Contains only assertions actually used. If other assertion is needed it has to be added into this file.
+"""
+
+interrupt_message = "Rest of test skipped as interrupt_test is set to True."
+
+
+def equal(value_1, value_2, assertion_message="", interrupt_test=False):
+	flag = check.equal(value_1, value_2, assertion_message)
+	if not flag and interrupt_test:
+		pytest.skip(interrupt_message)
+
+
+def is_true(boolean_value, assertion_message="", interrupt_test=False):
+	flag = check.is_true(boolean_value, assertion_message)
+	if not flag and interrupt_test:
+		pytest.skip(interrupt_message)
+
+
+def is_in(value_1, value_2, assertion_message="", interrupt_test=False):
+	flag = check.is_in(value_1, value_2, assertion_message)
+	if not flag and interrupt_test:
+		pytest.skip(interrupt_message)
+
+
+def greater(value_1, value_2, assertion_message="", interrupt_test=False):
+	flag = check.greater(value_1, value_2, assertion_message)
+	if not flag and interrupt_test:
+		pytest.skip(interrupt_message)
