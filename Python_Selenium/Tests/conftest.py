da6aff7ae36903ea64b27eea7de4f31f0796fa8a
OMGitH <malinik@volny.cz>
2023-11-27 20:25:15 +0100

    Added pytest html report, added folder with reports to .gitignore.

--- Python_Selenium/Tests/conftest.py
+++ Python_Selenium/Tests/conftest.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from Config.test_data import TestData
 import pytest
+import os
+from datetime import datetime
 
 
 @pytest.fixture
@@ -11,3 +13,21 @@ def initialize_driver(request):
     request.cls.driver = driver
     yield
     driver.quit()
+
+
+# Configuration of location and name of html report file.
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    report_folder = "Reports"
+    report_file = "Test_report_"+datetime.now().strftime("%d-%m-%Y_%H-%M-%S")+".html"
+    report_location = report_folder+"/"+report_file
+    if not os.path.exists(report_folder):
+        os.makedirs(report_folder)
+    config.option.htmlpath = report_location
+
+
+# Configuration of title of html report.
+def pytest_html_report_title(report):
+    report_title = "Test execution report, "+datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    report.title = report_title
+    
